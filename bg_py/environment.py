7632ec43c8872fa4e338c207e4868fc5324e36b9
Merlin Unterfinger <info@munterfinger.ch>
2021-01-03 11:29:10 +0100

    Add docstrings to log.py

--- bg_py/environment.py
+++ bg_py/environment.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-BG_LOG = Path("tmp/bg.log")
+BG_LOG_FILE = Path("tmp/bg.log")
 BG_COMM_FILE = Path("tmp/comm.fifo")
 BG_INTERVAL = 2
--- bg_py/log.py
+++ bg_py/log.py
@@ -1,12 +1,25 @@
 from datetime import datetime
 from os import getpid
-from environment import BG_LOG
+from environment import BG_LOG_FILE
 
 
-def clear_log():
-    open(BG_LOG, "w").close()
+def clear_log() -> None:
+    """
+    Clears the log file.
+    """
+    open(BG_LOG_FILE, "w").close()
 
 
-def write_log(message):
-    with open(BG_LOG, "a") as log:
-        log.write(f"{datetime.now().replace(microsecond=0)} - {getpid()}: {message}\n")
+def write_log(text: str) -> None:
+    """
+    Writes text to log file.
+
+    Parameters
+    ----------
+    text : str
+        Log entry text to write.
+    """
+    with open(BG_LOG_FILE, "a") as log:
+        log.write(
+            f"{datetime.now().replace(microsecond=0)} - {getpid()}: {text}\n"
+        )
