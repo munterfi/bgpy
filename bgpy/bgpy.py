101d7169a83b6bffb39a4141d532a33a701c9849
Merlin Unterfinger <info@munterfinger.ch>
2021-01-18 12:20:53 +0100

    Add interface.

---
+++ bgpy/bgpy.py
@@ -0,0 +1,63 @@
+from message import MessageType
+from sockets import ClientSocket, ServerSocket
+from interface import respond
+from sys import argv
+
+# Server
+HOST = argv[1]
+PORT = int(argv[2])
+INIT = False
+EXIT = False
+
+with ServerSocket(HOST, PORT) as ss:
+
+    while not EXIT:
+        sock = ss.accept()
+
+        with ClientSocket(sock=sock) as cs:
+
+            while True:
+
+                # Read messages
+                msg = cs.recv()
+                if msg is None:
+                    break
+
+                # Message type: INIT
+                if msg.type is MessageType.INIT:
+                    if INIT:
+                        print("Already initialised.")
+                        continue
+
+                    # Extract tasks from INIT message
+                    tasks = msg.get_args()
+                    init_task = tasks["init_task"]
+                    exec_task = tasks["exec_task"]
+                    exit_task = tasks["exit_task"]
+
+                    # Execute INIT task and setup init_args
+                    init_args = init_task(cs)
+
+                    # Set INIT to True to avoid second initialization
+                    INIT = True
+                    continue
+
+                # Message type: EXIT
+                if msg.type is MessageType.EXIT:
+
+                    # Execute exit_task, returns None
+                    _ = exit_task(cs, init_args, msg.get_args())
+
+                    # Set exit to True and trigger exit
+                    EXIT = True
+                    break
+
+                if not INIT:
+                    print("Initialize first!")
+                    continue
+
+                # Message type: EXEC
+                if msg.type is MessageType.EXEC:
+
+                    # Execute exec_task and overwrite init_args
+                    init_args = exec_task(cs, init_args, msg.get_args())
