82839fd948884ab22539fa90b2d68c077842bf2f
Merlin Unterfinger <info@munterfinger.ch>
2021-02-08 18:54:39 +0100

    Format tests.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,8 @@
 
 """Tests for `bgpy.cli` module."""
 
-from bgpy.core.environment import HOME, HOST, PORT  #, STARTUP_TIME
+from bgpy.core.environment import HOME, HOST, PORT  # , STARTUP_TIME
+
 # from time import sleep
 from subprocess import Popen
 
@@ -14,7 +15,7 @@ start = Popen(
 )
 
 # Wait to ensure client socket does not miss server socket
-#sleep(STARTUP_TIME)
+# sleep(STARTUP_TIME)
 
 # Terminate
 stop = Popen(
@@ -22,7 +23,7 @@ stop = Popen(
 )
 
 # Wait to ensure server socket is shut down, before next test starts
-#sleep(STARTUP_TIME)
+# sleep(STARTUP_TIME)
 
 
 def test_cli_success():
--- tests/test_client_server.py
+++ tests/test_client_server.py
@@ -2,10 +2,11 @@
 
 """Tests for `bgpy.client` and `bgpy.server` modules."""
 
-from bgpy.core.environment import PORT, HOST, HOME  #, STARTUP_TIME
+from bgpy.core.environment import PORT, HOST, HOME  # , STARTUP_TIME
 from bgpy.client import Client
 from bgpy.server import Server
 from bgpy.example.tasks import init_task, exec_task, exit_task
+
 # from time import sleep
 
 LOG_FILE = HOME / "test_workflow.log"
@@ -35,7 +36,7 @@ client.execute({"command": "decrease", "value_change": 100})
 # Terminate and wait for response, receive OK with values
 args = client.terminate(await_response=True)
 
-#sleep(STARTUP_TIME)
+# sleep(STARTUP_TIME)
 
 
 def test_request_count():
